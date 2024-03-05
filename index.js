6a8e3608667b8902d7909dd69a2845fa36b127af
Flipps12 <paginasypruebas12@gmail.com>
2024-03-05 19:14:27 -0300

    postgres in vercel

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-import express from 'express';
-
+const express = require('express')
+const path = require('path')
+const pg = require('pg')
 // Crear una nueva aplicación Express
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Definir un puerto para nuestro servidor
 const port = 3000 || process.env.PORT;
 
+
+const pool = new pg.Pool({
+    connectionString: process.env.POSTGRES_URL,
+})
+
 // Definir una ruta de prueba
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+app.get('hora', async (req, res) => {
+    fecha = await pool.query('SELECT NOW()')
+    res.send(fecha)
+})
 // Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
