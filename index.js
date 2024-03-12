caa029bc60f9e7dd7ec5e5c353c9be614906dd2b
Flipps12 <paginasypruebas12@gmail.com>
2024-03-12 12:22:48 -0300

    a

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const path = require('path')
 const { Pool } = require('pg')
 require('dotenv').config()
 // Crear una nueva aplicación Express
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Definir un puerto para nuestro servidor
 const port = 3000 || process.env.PORT;
 
 
 const pool = new Pool({
-    connectionString: "postgres://default:HF9aO5Nkjiyz@ep-late-heart-a4xxfre5-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    connectionString: process.env.KEY_DATABASE,
+    ssl: true,
 })
 
 // Definir una ruta de prueba
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.get('/hora', async (req, res) => {
-    fecha = await pool.query('SELECT NOW()')
-    console.log('peticion:')
-    console.log(fecha)
-    res.send(fecha)
+app.get('/is', async (req, res) => {
+    fecha = await pool.query('SELECT * FROM usuarios')
+    console.log(fecha.rows)
+    res.sendFile(path.join(__dirname, 'public', 'sesion', 'create.html'));
 })
+app.post('/is', async (req, res) => {
+    const user = req.body.user;
+    const password = req.body.password;
+    console.log(user, password);
+    insert_user = await pool.query('INSERT INTO usuarios (nombre_usuario, contrasena) VALUES ($1, $2)', [user, password]);
+    console.log(insert_user)
+    //fecha = await pool.query('')
+})
+
 // Iniciar el servidor
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
